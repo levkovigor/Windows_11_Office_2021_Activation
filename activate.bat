3b918a2df7f2a15f70a81dcb397449b798a90ad1
levkovigor <levkov.igor@gmail.com>
2025-03-08 23:20:43 +0200

    Update activate.bat

--- activate.bat
+++ activate.bat
@@ -1,8 +1,8 @@
 @echo off
-:: Batch file to upgrade and activate Windows 11 Pro
+:: Batch file to upgrade and activate Windows 11 Pro and activate Microsoft Office
 :: Run this script as Administrator
 
-echo Starting the process to upgrade to Windows 11 Pro...
+echo Starting the process to upgrade to Windows 11 Pro and activate Microsoft Office...
 
 :: Step 1: Uninstall the current product key
 echo Uninstalling the current product key...
@@ -42,5 +42,40 @@ cscript //B slmgr.vbs /skms kms8.msguides.com
 cscript //B slmgr.vbs /ato
 
 echo Activation complete! Check your settings to confirm Windows 11 Pro is activated.
+
+:: Step 7: Activate Microsoft Office
+echo Activating Microsoft Office...
+
+:: Check for Office 2016/2019/2021 installation and activate
+if exist "C:\Program Files\Microsoft Office\Office16\ospp.vbs" (
+    cd /d "C:\Program Files\Microsoft Office\Office16"
+    goto ActivateOffice
+)
+
+if exist "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" (
+    cd /d "C:\Program Files (x86)\Microsoft Office\Office16"
+    goto ActivateOffice
+)
+
+echo Microsoft Office not found. Skipping Office activation.
+goto End
+
+:ActivateOffice
+echo Installing Office licenses...
+for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do (
+    cscript ospp.vbs /inslic:"..\root\Licenses16\%%x"
+)
+
+echo Setting Office product key...
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+
+echo Setting KMS server...
+cscript ospp.vbs /sethst:kms.msgang.com
+
+echo Activating Office...
+cscript ospp.vbs /act
+
+:End
+echo Process complete! Check your Office activation status.
 pause
 exit
